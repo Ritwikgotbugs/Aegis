f69518ee1dfb9414cc8c9ef083ed67be45289a27
majorwinger1316 <ak4425@srmist.edu.in>
2024-04-08 12:29:58 +0530

    Added header and footer

---
+++ aegis/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Icon from '../assets/encrypt.png';
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+
+function header() {
+  return (
+    <div className='header'>
+      <div className='left_side'>
+        <img src={Icon} />
+        <h1>AEGIS</h1>
+      </div>
+      <div className='right_side'>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/test">Test</Link>
+      </div>
+    </div>
+  )
+}
+
+export default header
